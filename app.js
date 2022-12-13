0baceb3c562bd13236000bfe1c44f02d6caddb95
SiyuXiao8 <siyuxiao8@gmail.com>
2022-12-13 01:31:06 -0500

    stop delopying to heroku

--- app.js
+++ app.js
@@ -6,26 +6,26 @@ import usersController from "./controllers/users/users-controller.js";
 import reviewsController from "./controllers/reviews/reviews-controller.js";
 import favoriteController from "./controllers/favorite/favorite-controller.js";
 
-// const options = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     serverSelectionTimeoutMS: 5000,
-//     autoIndex: false,
-//     maxPoolSize: 10,
-//     socketTimeoutMS: 45000,
-//     family: 4
-// }
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false,
+    maxPoolSize: 10,
+    socketTimeoutMS: 45000,
+    family: 4
+}
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://localhost:27017/animeApp'
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+//     || 'mongodb://localhost:27017/animeApp'
 
-mongoose.connect(CONNECTION_STRING)
+mongoose.connect('mongodb://localhost:27017/animeApp', options)
     .then(r => console.log('connected')); // url from mongodb
 
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000','https://master--euphonious-manatee-4dc64a.netlify.app/']
+    origin: 'http://localhost:3000'
 }))
 
 app.use(session({
