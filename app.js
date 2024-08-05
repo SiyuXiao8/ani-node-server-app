1fe71599c5b7e1c7709cf1a6fa1fa19c0cc42000
SiyuXiao8 <siyuxiao8@gmail.com>
2024-08-05 17:03:59 -0400

    added mongodb cloud

--- app.js
+++ app.js
@@ -1,43 +1,52 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from "mongoose";
-import session from 'express-session';
-import usersController from "./controllers/users/users-controller.js";
-import reviewsController from "./controllers/reviews/reviews-controller.js";
-import favoriteController from "./controllers/favorite/favorite-controller.js";
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import session from 'express-session'
+import usersController from './controllers/users/users-controller.js'
+import reviewsController from './controllers/reviews/reviews-controller.js'
+import favoriteController from './controllers/favorite/favorite-controller.js'
 
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000,
-    autoIndex: false,
-    maxPoolSize: 10,
-    socketTimeoutMS: 45000,
-    family: 4
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  autoIndex: false,
+  maxPoolSize: 10,
+  socketTimeoutMS: 45000,
+  family: 4,
 }
+
+// store this in a env file
+const CONNECTION_STRING =
+  'mongodb+srv://siyuxiao:siyuxiao@cluster0.o47wv2i.mongodb.net/animeApp?retryWrites=true&w=majority'
 
 // const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
 //     || 'mongodb://localhost:27017/animeApp'
 
-mongoose.connect('mongodb://localhost:27017/animeApp', options)
-    .then(r => console.log('connected')); // url from mongodb
+mongoose
+  .connect(CONNECTION_STRING, options)
+  .then((r) => console.log('connected')) // url from mongodb
 
-const app = express();
-app.use(cors({
+const app = express()
+app.use(
+  cors({
     credentials: true,
-    origin: 'http://localhost:3000'
-}))
+    origin: 'http://localhost:3000',
+  })
+)
 
-app.use(session({
+app.use(
+  session({
     secret: 'should be an environment variable',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }
-}))
+    cookie: { secure: false },
+  })
+)
 app.use(express.json())
 
 usersController(app)
 reviewsController(app)
 favoriteController(app)
 
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4000)
